4450889fee0a339185f568ecaa759b4c333fa155
Blame Game <96299474+wujiafu007@users.noreply.github.com>
2024-12-10 15:28:47 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-#python3 -m torch.distributed.launch --nproc_per_node=8   train.py   /youtu_fuxi_team1_ceph/vtzhang/codes/data/imagenet   --epochs 300 --img-size 224 --drop-path 0.2 --lr 1e-3  --weight-decay 0.05 --aa rand-m9-mstd0.5-inc1 --warmup-lr 1e-6 --warmup-epochs 5  --output save --min-lr 1e-6  --model pvg_l    -b 40 --experiment None
-
-python3 -m torch.distributed.launch --nproc_per_node=8   train.py   /youtu_fuxi_team1_ceph/vtzhang/codes/data/imagenet   --epochs 300 --img-size 224 --drop-path 0.2 --lr 1e-3  --weight-decay 0.05 --aa rand-m9-mstd0.5-inc1 --warmup-lr 1e-6 --warmup-epochs 5  --output save --min-lr 1e-6  --model pvg_s    -b 104 --experiment None
+python3 -m torch.distributed.launch --nproc_per_node=8   train.py  data/imagenet   --epochs 300 --img-size 224 --drop-path 0.2 --lr 1e-3  --weight-decay 0.05 --aa rand-m9-mstd0.5-inc1 --warmup-lr 1e-6 --warmup-epochs 5  --output save --min-lr 1e-6  --model pvg_s    -b 104 --experiment None
 
